1eeda64fdee66b94e8b137ff608b8f6a4606f1f9
Falsitus <gelabertgalmes98@gmail.com>
2022-10-27 13:32:19 +0200

    Added Deploy script and Planet dropdown is working

--- server/src/app.js
+++ server/src/app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/planets', planetsRouter);
 app.use('/launches', launchesRouter);
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
