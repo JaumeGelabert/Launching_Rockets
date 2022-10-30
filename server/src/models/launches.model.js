8535e854cc242239ee532c76c3f6cec4aa5f7df7
Falsitus <gelabertgalmes98@gmail.com>
2022-10-30 17:16:40 +0100

    GetLatestFlightNumber y saveLaunch mediante Mongo

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,7 @@
 const launchesDatabase = require('./launches.mongo');
+const planets = require('./plantes.mongo');
+
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launches = new Map();
 
@@ -9,7 +12,7 @@ const launch = {
   mission: 'Nombre mision',
   rocket: 'Rocket',
   launchDate: new Date('December 27, 2030'),
-  target: 'Destination',
+  target: 'Kepler-1652 b',
   customers: ['Customer 1', 'Customer 2'],
   upcoming: true,
   success: true,
@@ -22,11 +25,30 @@ function existsLaunchWithId(launchId) {
   return launches.has(launchId);
 }
 
+async function getLatestFlightNumber() {
+  // .sort() ordena de menor a mayor, por eso añadimos el '-' y cojemos el primero
+  const latestLaunch = await launchesDatabase.findOne().sort('-fligthNumber');
+
+  if (!latestLaunch) {
+    return DEFAULT_FLIGHT_NUMBER;
+  }
+
+  return latestLaunch.flightNumber;
+}
+
 async function getAllLaunches() {
   return await launchesDatabase.find({}, { _id: 0, __v: 0 });
 }
 
 async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error('No matching planet found');
+  }
+
   await launchesDatabase.updateOne(
     {
       flightNumber: launch.flightNumber,
